2e4fbca8c5327e671e17ea0920db253ed52b457c
Franco Bottaro <60520589+francoabottaro@users.noreply.github.com>
2024-07-22 13:16:53 -0300

    correction UserRepository

--- domain/UserRepository.ts
+++ domain/UserRepository.ts
@@ -4,7 +4,7 @@ import { UserId } from "./UserId";
 export interface UserRepository {
   create(user: User): Promise<void>;
   getAll(): Promise<User[]>;
-  getOneById(id: UserId): Promise<User> | null;
+  getOneById(id: UserId): Promise<User | null>;
   edit(user: User): Promise<void>;
   delete(id: UserId): Promise<void>;
 }
